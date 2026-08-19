e5491537eaf4ef7fea1124934befac26d366856a
alexanderGrimaldo <1039824@my.scccd.edu>
2026-08-19 10:47:47 -0700

    Add basic hello world program in C++
    Initial implementation of a basic hello world program in C++ that outputs two lines.

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+//=========================================================
+//  File Name:      helloWorld.cpp
+//  Author:         Alexander Grimaldo
+//  Date:           August 17th, 2026
+//  Course:         CIT-66 C++ Programming
+//  Instructor:     Professor Mohle
+//  Description:    It's hello world, pretty basic stuff.
+//=======================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\n\n Hello World!" << "\n\n";
+    cout << "This is another line";
+    return 0;
+}
